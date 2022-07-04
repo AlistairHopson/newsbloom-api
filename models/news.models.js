c23d54b0a1582c3569d5b41f8a02b421723bf448
AlistairHopson <hopson.alistair@gmail.com>
2022-07-04 15:15:39 +0100

    add GET /api/topics and handle 404 and 500 errors with removed console log

--- models/news.models.js
+++ models/news.models.js
@@ -5,7 +5,6 @@ exports.selectTopics = () => {
   return db
     .query("SELECT * FROM topics;")
     .then((results) => {
-      console.log(results.rows);
       return results.rows;
     })
     .catch((err) => {
