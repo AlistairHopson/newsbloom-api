e05d1024e37afa1b99257e93fc8dbf842e981770
AlistairHopson <hopson.alistair@gmail.com>
2022-07-25 10:21:45 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const apiRouter = require("./routes/apiRouter");
