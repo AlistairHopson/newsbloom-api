33cd972337ffea1ffdf8c781c7e215d2e287d038
AlistairHopson <hopson.alistair@gmail.com>
2022-11-28 21:55:24 +0000

    amend post-heroku update

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,6 @@
 const { Pool } = require("pg");
-const ENV = process.env.NODE_ENV || "development";
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
+const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -18,5 +9,12 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
 
 module.exports = new Pool(config);
